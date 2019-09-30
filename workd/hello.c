c9f6e211365eb48eb4d268472326eb80a4d3366d
user <ashithosh77@gamil.com>
2019-09-30 11:56:05 +0300

    20190930_11_56

---
+++ workd/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// this library should be added for printf
+
+void main()
+ {
+// Hello world...!; // absolutely wrong!
+  "Hello world"; // working, but text does not appear on screen
+ printf("Hello world...!\n");
+ }
+
+
