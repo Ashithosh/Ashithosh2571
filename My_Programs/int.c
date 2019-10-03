3b560e0d4063a6f0a51ff5bc91030fdd2bebdb01
Ashithosh <ashithosh77@gamil.com>
2019-10-03 21:57:54 +0300

    My_programs

---
+++ My_Programs/int.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+ int a;
+ int b;
+
+printf("enter the first number:");
+scanf("%d",&a);
+
+printf("enter the second number:");
+scanf("%d",&b);
+
+int c=a+b;
+printf("sum of two numbers is:%d",c);
+
+}
