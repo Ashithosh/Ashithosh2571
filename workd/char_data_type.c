24ca0b8a8f89b0b4b6579a8cd92e1ab606b358fc
user <ashithosh77@gamil.com>
2019-10-21 14:10:26 +0300

    20191021_14_10

---
+++ workd/char_data_type.c
@@ -0,0 +1,13 @@
+#include<stdio.h> // we will use printf and scanf
+ 
+ void main() // main function is "entry point" and it should obligatory
+{
+ char b=127;
+ printf("b char variable value as decimal number (?127): %d\n,b);
+
+ char C=128;
+ printf("c char variable value as decimal number (?128): %d\n,b);
+
+ char d=129;
+ printf("c char variable value as decimal number (?129): %d\n,b);
+}
