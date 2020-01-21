1b7facbf3e0102ffa1f43c54b6e0fe6c96c88f49
root <ashithosh77@gamil.com>
2020-01-21 12:59:39 -0500

    20200121_20_00

--- lab_works/1lw_series/sinh5.c
+++ lab_works/1lw_series/sinh5.c
@@ -2,29 +2,28 @@
 #include<math.h>
 void main()
 {
- double x,y,a,S;
+ long double x,y,a,S;
  int k=0;
  printf("Enter Value For X\n");
- scanf("%lf",&x); 
+ scanf("%Lf",&x); 
  y = sinh(x);
- printf("y=sinh(%.2f)=%.2f\n",x,y);
+ printf("y=sinh(%.2Lf)=%.2Lf\n",x,y);
 
  a = (1)*pow(x,2*k+1)/(1.);
- S += a;
- printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
+ S = a;
+ printf("%.2Lf\t%8.2Lf\t%8.2Lf\n",x,a,S);
 
-  while(k<500)
+while (k<500)
  {
   if (k == 0)
-  printf("a0 =%e\tS0 =%2f\n",x,a,S);
+   printf("a0 = %Le\tS0 =%.2Lf\n",a,S);
   k++;
   a = a * (1) *x*x / ((2*k)*(2*k+1));
   S += a;
-  printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
   if (k == 499)
-  printf("a499 = %e\tS499 =%.2f\n",a,S);
+  printf("a499 = %Le\tS499 =%.2Lf\n",a,S);
  }
-  printf("a500 =%e\tS500 =%.2f\n",a,S);
+  printf("a500 = %Le\tS500 =%.2Lf\n",a,S);
 
  printf("\n");
  printf("\t\t          500\n");
@@ -42,3 +41,4 @@ void main()
  printf("R=\t\t   -----------\n");
  printf("\t\t\tk\n");
 }
+
