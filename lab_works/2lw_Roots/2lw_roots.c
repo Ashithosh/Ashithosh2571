b1bc3557f50d407d3487b89d62cd7d3ce0eb85e0
root <ashithosh77@gamil.com>
2020-01-23 16:42:52 -0500

    20200123_23_43

--- lab_works/2lw_Roots/2lw_roots.c
+++ lab_works/2lw_Roots/2lw_roots.c
@@ -1,26 +1,41 @@
 #include<stdio.h>
 #include<math.h>
 
-float modified_sin(float x, float A)
-{
- return sinh(x)-A;
-}
+int main() {
+ float a,c ,b ,x ,delta_x,funkca, funkcb, funkcx;
+ int i=0;
 
-void main()
-{
- float a,x,delta_x,b,y,A;
- a = 0;
- b = 2*M_PI;
+  printf("Calculating the root of a function: \n");
 
- printf("Dear User, Please enter a value of A for the following equation: sinh(x)=A\n");
- scanf("%f",&A);
- x = a;
- delta_x = 0.1;
- printf("\tx\ty\n");
- while(x<b)
- {
- printf("%10.1f%10.1f\n",x,modified_sinh(x,A));
+  printf("User, please enter start limit: ");
+  scanf ("%f", &a);
 
- x += delta_x;
+  printf("User, please enter end limit: ");
+  scanf ("%f", &b);
+
+  printf("User please enter accuracy: ");
+  scanf ("%e", &delta_x);
+
+  printf("User, please enter for what y you can calculate x: ");
+  scanf ("%f", &c);
+  funkca = (1+a)*sinh(a) - c; funkcb = (1+b)*sinh(b) - c;
+
+  printf("(1+%7.3f)*sinh(%7.3f) - %7.3f =%7.3f\t\t\t\t\t\t\t",a,a,c,funkca);
+  printf("(1+%7.3f)*(%7.3f) - %7.3f =%7.3f\n",b,b,c,funkcb);
+
+ while ((b-a)>delta_x){
+  x = (a+b)/2.;
+  if(funkca*((1+x)*sinh(x)-c)>0)
+   a = x;
+  else
+   b = x;
+  printf("(1+%7.3f)*sinh(%7.3f) - %7.3f =%7.3f\t",a,a,c,funkca);
+  printf("(1+%7.3f)*sinh(%7.3f) - %7.3f =%7.3f\t",b,b,c,funkcb);
+  printf("(1+%7.3f)*sinh(%7.3f) - %7.3f =%7.3f\n",x,x,c,(1+x)*sinh(x)-c);
+   i++;
  }
+
+ printf("\nNumber of iterations: %d\n",i);
+ printf("The start is at x = %.5f because (1 + x) * sinh(x) is %.3f\n",x,(1 + x)*sinh(x));
+ return 0;
 }
